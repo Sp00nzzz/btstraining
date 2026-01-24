48a9945e423c8c23ab5509e54a91d8d27130ac61
Sp00nzzz <mike.nguyen992004@gmail.com>
2026-01-24 14:35:27 -0500

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter, Pixelify_Sans, Nunito } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import AppShell from "@/components/AppShell";
 
@@ -45,6 +46,7 @@ export default function RootLayout({
         <AppShell>
           {children}
         </AppShell>
+        <Analytics />
       </body>
     </html>
   );
